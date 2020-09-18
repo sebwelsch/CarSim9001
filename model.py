8422e8e6c3a8d5d20c5fc44a3b0dfd828d26b872
sebwelsch <sebjak1@hotmail.com>
2020-09-18 08:31:00 +0200

    Made shifting

---
+++ model.py
@@ -0,0 +1,33 @@
+class Gearbox(object)
+
+    def __init__(self):
+        currentGear = 0
+        clutchEngaged = False
+        gears = [0, 0.8, 1, 1.4, 2.2, 3.8]
+
+    def shiftUp(self):
+        if clutchEngaged = True:
+            return
+        elif currentGear < 5:
+            currentGear = + 1
+        else:
+            return
+
+    def shiftDown(self):
+        if clutchEngaged = True:
+            return
+        elif currentGear > 0:
+            currentGear = - 1
+        else:
+            return
+
+class Wheel(object)
+
+
+class Engine(object)
+
+
+class Car(object)
+
+
+class Tank(object)
